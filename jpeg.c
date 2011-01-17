535acb4b15f499330998c3d1c514108ae7e63d19
Matus Tejiscak <ziman@functor.sk>
2011-01-17 16:01:09 +0100

    Copyright notices added.

--- jpeg.c
+++ jpeg.c
@@ -1,3 +1,8 @@
+/*
+ * jpg-recover -- Extract JPEG/CR2 files from raw bytes (filesystem images etc.).
+ * Copyright (c) 2011, Matus Tejiscak <functor.sk@ziman>.
+ * Released under the BSD license: http://creativecommons.org/licenses/BSD/
+ */
 #include "jpeg.h"
 
 /** Maximum size of the SOS-EOI block, in bytes. */
